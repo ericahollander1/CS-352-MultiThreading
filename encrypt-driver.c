06e286e21dcbb3068198d5941852f3e5933b1bf0
ljenkins <ljenkins@iastate.edu>
2022-04-20 17:55:48 -0500

    Skeleton of all implementations except for reset

--- encrypt-driver.c
+++ encrypt-driver.c
@@ -9,7 +9,10 @@
     Authors: Erica Hollander & Lakin Jenkins
 */
 //buffer = sizeof(char)
-
+sem_t *sem_char_render;
+sem_t *sem_count_in;
+sem_t *sem_count_out;
+int render_count;
 typedef struct circular_buf_t {
     uint8_t * buffer;
     size_t head;
@@ -23,27 +26,95 @@ circular_buf_t *input_buf = malloc(sizeof *input_buf);
 circular_buf_t *output_buf = malloc(sizeof *output_buf);
 
 void reset_requested() {
-	log_counts();
+    // stop the reader thread from reading any more input
+    // make sure it is safe to reset
+    log_counts();
+}
+void rest_finished() {
+    // resume the reader thread
 }
 
-void reset_finished() {
-}
-int getInputCircBufSize(){
-    if(input_buf->head >= input_buf->tail){
+size_t getCircBufSize(circular_buf_t buf){
+    size_t size = buf->max;
+    if(!buf->full)
+    {
+        if(buf->head >= buf->tail)
+        {
+            size = (buf->head - buf->tail);
+        }
+        else
+        {
+            size = (buf->max + buf->head - buf->tail);
+        }
+    }
+return size;
 
+}
+static void addValue(circular_buf_t buf, uint8_t data, sem_t sem_type){
+    if(buf->full)
+    {
+        sem_wait(sem_type);
+        buf->tail = (buf->tail + 1) % buf->max;
+    }
+
+    //buf->head = (buf->head + 1) % buf->max;
+    buf->full = (buf->head == buf->tail + 1);
+    buf->buffer[buf->tail] = data;
+
+}
+static void moveHead(circular_buf_t buf){
+    if(buf->head != buf->tail)
+    {
+        buf->head = (buf->head + 1) % buf->max;
+        buf->full = (buf->head == buf->tail+1);
     }
 
 }
+int getIndexOfCircBuf(circular_buf_t buf, int behind){
+    int index = buf->tail;
+    if(buf->tail-behind < 0){
+        index = buf->max+(buf->tail-behind);
+    }
+    else{
+        index = buf->tail-behind;
+    }
+    return index;
+}
 
 void *renderThread(void *vargp){
-        if(input_buf->full){
-            sem_wait(sem_char_read);
-        }
-    read_input();
-        if(input_buf->head-input_buf->tail){
+    addValue(input_buf, read_input(), sem_char_render);
+    render_count++;
+    sem_post(sem_count_in);
+}
 
-        }
+void *inputCounterThread(void *vargp){
+    sem_wait(sem_count_in);
+    if(render_count > get_input_total_count()){ //maybe we dont need this if cause we use sems
+        count_input(input_buf->buf[getIndexOfCircBuf(input_buf, render_count-get_input_total_count())]);
+    }
+}
+void *encryptThread(void *vargp){
+    if(render_count-get_input_total_count() < input_buf->max && !output_buf->full){ // might need to be equal if issues debug there
+        //encrypt();//count_input(input_buf->buf[getIndexOfCircBuf(input_buf, get_input_total_count()-render_count)]);
+        addValue(output_buf, encrypt(), sem_count_out);
+        moveHead(input_buf);
+        sem_post(sem_char_render);
+    }
+}
 
+void *outputCounterThread(void *vargp){
+    if(writer_count < get_output_total_count()){ //maybe we dont need this if cause we use sems
+        count_output(input_buf->buf[getIndexOfCircBuf(output_buf, get_output_total_count()-writer_count)]);
+    }
+}
+
+void *writerThread(void *vargp){
+    writer_count++;
+    if(output_buf->head != tail){
+        write_output(output_buf->head);
+        moveHead(output_buf);
+        sem_post(sem_count_out);
+    }
 }
 
 int main(int argc, char *argv[]) {
@@ -74,9 +145,16 @@ int main(int argc, char *argv[]) {
     output_buf->tail = 0;
 
     pthread_t tid;
-    sem_char_read = sem_open("/sem_test_render", O_CREAT, 0644, 0);
+    sem_char_render = sem_open("/sem_test_render", O_CREAT, 0644, 0);
     sem_unlink("/sem_test_render");
+    sem_count_in = sem_open("/sem_test_count_in", O_CREAT, 0644, 0);
+    sem_unlink("/sem_test_count_in");
+    sem_count_out = sem_open("/sem_test_count_out", O_CREAT, 0644, 0);
+    sem_unlink("/sem_test_count_out");
     pthread_create(&tid, NULL, renderThread, (void *)&tid);
+    pthread_create(&tid, NULL, countInThread, (void *)&tid);
+    pthread_create(&tid, NULL, countOutThread, (void *)&tid);
+
     //pthread_exit(NULL);
 
 	while ((c = read_input()) != EOF) {
