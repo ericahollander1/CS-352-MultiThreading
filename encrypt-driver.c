7f36ae987419107fe29c299f3adb69d131958960
ljenkins <ljenkins@iastate.edu>
2022-04-20 16:05:32 -0500

    Some implementation for renderthread

--- encrypt-driver.c
+++ encrypt-driver.c
@@ -9,6 +9,7 @@
     Authors: Erica Hollander & Lakin Jenkins
 */
 //buffer = sizeof(char)
+
 typedef struct circular_buf_t {
     uint8_t * buffer;
     size_t head;
@@ -17,11 +18,32 @@ typedef struct circular_buf_t {
     bool full;
 }circular_buf_t;
 
+circular_buf_t *input_buf = malloc(sizeof *input_buf);
+
+circular_buf_t *output_buf = malloc(sizeof *output_buf);
+
 void reset_requested() {
 	log_counts();
 }
 
 void reset_finished() {
+}
+int getInputCircBufSize(){
+    if(input_buf->head >= input_buf->tail){
+
+    }
+
+}
+
+void *renderThread(void *vargp){
+        if(input_buf->full){
+            sem_wait(sem_char_read);
+        }
+    read_input();
+        if(input_buf->head-input_buf->tail){
+
+        }
+
 }
 
 int main(int argc, char *argv[]) {
@@ -38,26 +60,27 @@ int main(int argc, char *argv[]) {
     }
     printf("input buffer %d\n", N);
     printf("output buffer %d\n", M);
-	circular_buf_t *input_buf = malloc(sizeof *input_buf);
+
 	input_buf->buffer = sizeof(char);
 	input_buf->max = N;
 	input_buf->full = 0;
 	input_buf->head = 0;
 	input_buf->tail = 0;
 
-    circular_buf_t *output_buf = malloc(sizeof *output_buf);
     output_buf->buffer = sizeof(char);
     output_buf->max = M;
     output_buf->full = 0;
     output_buf->head = 0;
     output_buf->tail = 0;
 
-/*    pthread_t tid;
-    pthread_create(&tid, NULL, myThreadFun, (void *)&tid);
-    pthread_exit(NULL);*/
+    pthread_t tid;
+    sem_char_read = sem_open("/sem_test_render", O_CREAT, 0644, 0);
+    sem_unlink("/sem_test_render");
+    pthread_create(&tid, NULL, renderThread, (void *)&tid);
+    //pthread_exit(NULL);
 
 	while ((c = read_input()) != EOF) {
-		count_input(c);
+	    count_input(c);
 		c = encrypt(c);
 		count_output(c);
 		write_output(c);
