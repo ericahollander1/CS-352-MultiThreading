6093e20f608ce5a92ec16cc13901bd73586ec8ab
elh1 <elh1@iastate.edu>
2022-04-24 15:19:24 -0500

    wip

--- encrypt-driver.c
+++ encrypt-driver.c
@@ -4,6 +4,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <pthread.h>
+#include <semaphore.h>
 
 /*
     Authors: Erica Hollander & Lakin Jenkins
@@ -12,7 +13,7 @@
 sem_t *sem_char_render;
 sem_t *sem_count_in;
 sem_t *sem_count_out;
-int render_count;
+int render_count, writer_count;
 typedef struct circular_buf_t {
     uint8_t * buffer;
     size_t head;
@@ -21,20 +22,17 @@ typedef struct circular_buf_t {
     bool full;
 }circular_buf_t;
 
-circular_buf_t *input_buf = malloc(sizeof *input_buf);
-
-circular_buf_t *output_buf = malloc(sizeof *output_buf);
+circular_buf_t *input_buf;
+circular_buf_t *output_buf;
 
 void reset_requested() {
-    // stop the reader thread from reading any more input
-    // make sure it is safe to reset
     log_counts();
 }
-void rest_finished() {
-    // resume the reader thread
+
+void reset_finished() {
 }
 
-size_t getCircBufSize(circular_buf_t buf){
+size_t getCircBufSize(circular_buf_t *buf){
     size_t size = buf->max;
     if(!buf->full)
     {
@@ -50,7 +48,7 @@ size_t getCircBufSize(circular_buf_t buf){
 return size;
 
 }
-static void addValue(circular_buf_t buf, uint8_t data, sem_t sem_type){
+static void addValue(circular_buf_t *buf, uint8_t data, sem_t *sem_type){
     if(buf->full)
     {
         sem_wait(sem_type);
@@ -62,7 +60,7 @@ static void addValue(circular_buf_t buf, uint8_t data, sem_t sem_type){
     buf->buffer[buf->tail] = data;
 
 }
-static void moveHead(circular_buf_t buf){
+static void moveHead(circular_buf_t *buf){
     if(buf->head != buf->tail)
     {
         buf->head = (buf->head + 1) % buf->max;
@@ -70,7 +68,7 @@ static void moveHead(circular_buf_t buf){
     }
 
 }
-int getIndexOfCircBuf(circular_buf_t buf, int behind){
+int getIndexOfCircBuf(circular_buf_t *buf, int behind){
     int index = buf->tail;
     if(buf->tail-behind < 0){
         index = buf->max+(buf->tail-behind);
@@ -90,13 +88,13 @@ void *renderThread(void *vargp){
 void *inputCounterThread(void *vargp){
     sem_wait(sem_count_in);
     if(render_count > get_input_total_count()){ //maybe we dont need this if cause we use sems
-        count_input(input_buf->buf[getIndexOfCircBuf(input_buf, render_count-get_input_total_count())]);
+        count_input(input_buf->buffer[getIndexOfCircBuf(input_buf, render_count-get_input_total_count())]);
     }
 }
 void *encryptThread(void *vargp){
     if(render_count-get_input_total_count() < input_buf->max && !output_buf->full){ // might need to be equal if issues debug there
         //encrypt();//count_input(input_buf->buf[getIndexOfCircBuf(input_buf, get_input_total_count()-render_count)]);
-        addValue(output_buf, encrypt(), sem_count_out);
+        addValue(output_buf, encrypt(input_buf->buffer[input_buf->head]), sem_count_out);
         moveHead(input_buf);
         sem_post(sem_char_render);
     }
@@ -104,18 +102,29 @@ void *encryptThread(void *vargp){
 
 void *outputCounterThread(void *vargp){
     if(writer_count < get_output_total_count()){ //maybe we dont need this if cause we use sems
-        count_output(input_buf->buf[getIndexOfCircBuf(output_buf, get_output_total_count()-writer_count)]);
+        count_output(input_buf->buffer[getIndexOfCircBuf(output_buf, get_output_total_count()-writer_count)]);
     }
 }
 
 void *writerThread(void *vargp){
     writer_count++;
-    if(output_buf->head != tail){
+    if(output_buf->head != output_buf->tail){
         write_output(output_buf->head);
         moveHead(output_buf);
         sem_post(sem_count_out);
     }
 }
+
+//void mallocCircularBuf(*circular_buf_t buffer, int size) {
+//    buffer->max = size;
+//    buffer->buffer = (uint8_t *)malloc(sizeof(uint8_t) * size);
+//    buffer->tail = malloc(sizeof(size_t));
+//    buffer->head = malloc(sizeof(size_t));
+//    buffer->full = 0;
+//    buffer-> tail = 0;
+//    buffer-> head = 0;
+//}
+
 
 int main(int argc, char *argv[]) {
 	init("in.txt", "out.txt", "log.txt");
@@ -131,38 +140,45 @@ int main(int argc, char *argv[]) {
     }
     printf("input buffer %d\n", N);
     printf("output buffer %d\n", M);
+//    input_buf = cb_init(input_buf, N);
+//    output_buf = cb_init(output_buf, M);
+    input_buf = malloc(sizeof(bool) + sizeof(size_t)*3 + sizeof(uint8_t)*N);
+    output_buf = malloc(sizeof(bool) + sizeof(size_t)*3 + sizeof(uint8_t)*M);
 
-	input_buf->buffer = (uint8_t *)malloc(N * sizeof(uint8_t));
+    input_buf->buffer = (uint8_t *)malloc(N * sizeof(uint8_t));
 	input_buf->max = N;
 	input_buf->full = 0;
 	input_buf->head = 0;
 	input_buf->tail = 0;
+
+	printf("got here");
 
     output_buf->buffer = (uint8_t *)malloc(M * sizeof(uint8_t));
     output_buf->max = M;
     output_buf->full = 0;
     output_buf->head = 0;
     output_buf->tail = 0;
+    printf("got here 2");
+
 
     pthread_t tid;
+    printf("tid");
+
     sem_char_render = sem_open("/sem_test_render", O_CREAT, 0644, 0);
     sem_unlink("/sem_test_render");
     sem_count_in = sem_open("/sem_test_count_in", O_CREAT, 0644, 0);
     sem_unlink("/sem_test_count_in");
     sem_count_out = sem_open("/sem_test_count_out", O_CREAT, 0644, 0);
     sem_unlink("/sem_test_count_out");
+    printf("sems created");
+
     pthread_create(&tid, NULL, renderThread, (void *)&tid);
-    pthread_create(&tid, NULL, countInThread, (void *)&tid);
-    pthread_create(&tid, NULL, countOutThread, (void *)&tid);
+    pthread_create(&tid, NULL, inputCounterThread, (void *)&tid);
+    pthread_create(&tid, NULL, outputCounterThread, (void *)&tid);
+    printf("pthread create");
+
 
     //pthread_exit(NULL);
-
-	while ((c = read_input()) != EOF) {
-	    count_input(c);
-		c = encrypt(c);
-		count_output(c);
-		write_output(c);
-	}
 	printf("End of file reached.\n");
 	log_counts();
 }
